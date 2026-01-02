a7beb4ac9665199f2910e38237a8b0803fbe9522
jtave111 <jtave.111@proton.me>
2026-01-02 00:55:15 -0300

    Echo TCP server concluido

--- SpiderNETx/server/tcp/EchoServer.cpp
+++ SpiderNETx/server/tcp/EchoServer.cpp
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h>
+#include <cstdio>
+#include <cstring>
+
 void EchoServer::startEchoTcpServer(char * ip, int port){
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
@@ -22,9 +25,16 @@ void EchoServer::startEchoTcpServer(char * ip, int port){
     server.sin_family = AF_INET;
     server.sin_port = htons(port);
     inet_pton(AF_INET, ip, &server.sin_addr);
-    socklen_t server_len = sizeof(server);
+
     
-    bind(sock, (struct sockaddr *)&server, sizeof(server));
+    if(bind(sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+
+        fprintf(stderr, "[x] Blind error");
+        close(sock);
+        return;
+
+    }
+
 
     int ls =  listen(sock, 100);  
 
@@ -42,10 +52,51 @@ void EchoServer::startEchoTcpServer(char * ip, int port){
 
         int socket_agent = accept(sock, (struct sockaddr *)&client, &client_len);
 
+
+        if(socket_agent > 0){
+
+            while (true){
+                    
+                char  buffer [255];
+                memset(buffer, 0, sizeof(buffer));
+
+
+                int bytes_client = recv(socket_agent, buffer, sizeof(buffer) -1, 0);
+
+                if(bytes_client > 0){
+                    
+                    buffer[bytes_client] = '\0';
+
+                    send(socket_agent, buffer, bytes_client, 0);
+
+
+                }else if(bytes_client == 0){
+                    
+                    printf("[*] Client disconected\n");
+
+                    break;
+
+                }else{
+
+                    fprintf(stderr, "[X]Error recv . . \n");
+                    
+                    break;
+                }
+
+                
+            }
+            close(socket_agent);
+            
+        }else{
+
+            printf("[x] Accept Error . . \n");
+            continue;
+
+        }
+
     }
     
-
-    //Create step 6(eco talking) - 7(finished),
+    close(sock);
 
 
 }
